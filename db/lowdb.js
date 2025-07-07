8f90694dec2bfa9124a39402a66cc85db04b8037
Ikhtiyor Abdukhoshimov <i.abdukhoshimov@gmail.com>
2025-07-08 00:12:34 +0500

    Fix module compatibility for bot commands and database

--- db/lowdb.js
+++ db/lowdb.js
@@ -1,11 +1,11 @@
 // db/lowdb.js
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
-import path from 'path'
+const { Low } = require('lowdb');
+const { JSONFile } = require('lowdb/node');
+const path = require('path');
 
 const file = path.resolve('./db/posts.json')
 const adapter = new JSONFile(file)
-const db = new Low(adapter)
+const db = new Low(adapter, { posts: {} })
 
 async function initDB() {
   await db.read()
@@ -17,24 +17,31 @@ async function initDB() {
   }
 }
 
-export async function setPost(name, content) {
+async function setPost(name, content) {
   await initDB()
   db.data.posts[name] = content
   await db.write()
 }
 
-export async function getPosts() {
+async function getPosts() {
   await initDB()
   return db.data.posts
 }
 
-export async function getPost(name) {
+async function getPost(name) {
   await initDB()
   return db.data.posts[name]
 }
 
-export async function deletePost(name) {
+async function deletePost(name) {
   await initDB()
   delete db.data.posts[name]
   await db.write()
 }
+
+module.exports = {
+  setPost,
+  getPosts,
+  getPost,
+  deletePost,
+};
